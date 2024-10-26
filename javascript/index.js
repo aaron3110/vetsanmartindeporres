a114e8d6c632e3a4893e004f9401d6b229df2f55
juego <150973333+aaron3110@users.noreply.github.com>
2024-10-26 00:19:44 -0600

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const elementsToAnimate = document.querySelectorAll('section, img, h1, h2, h3, p, div, .overlay, .header-text, header');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animated');
+                if (entry.target.tagName === 'IMG' || entry.target.tagName === 'DIV' || entry.target.tagName === 'SECTION' || entry.target.classList.contains('overlay') || entry.target.classList.contains('header-text') || entry.target.tagName === 'HEADER') {
+                    entry.target.classList.add('fade-in');
+                } else {
+                    entry.target.classList.add('slide-up');
+                }
+                if (entry.target.tagName === 'HEADER') {
+                    entry.target.classList.add('slide-down');
+                }
+            } else {
+                entry.target.classList.remove('animated', 'fade-in', 'slide-up', 'slide-down');
+            }
+        });
+    });
+
+    elementsToAnimate.forEach(element => {
+        observer.observe(element);
+    });
+});
+
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
