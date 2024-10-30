89eca727180929601dd1dacd87264a63f8600a6c
juego <150973333+aaron3110@users.noreply.github.com>
2024-10-30 11:54:22 -0600

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -63,3 +63,17 @@ document.addEventListener('DOMContentLoaded', (event) => {
         });
     }
 });
+
+   document.addEventListener('DOMContentLoaded', function() {
+        const videos = document.querySelectorAll('.video');
+
+        videos.forEach(video => {
+            video.addEventListener('play', function() {
+                videos.forEach(v => {
+                    if (v !== video) {
+                        v.pause();
+                    }
+                });
+            });
+        });
+    });
